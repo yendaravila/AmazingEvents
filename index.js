38344195d6984ba141ca3b2e1b3a12174ef8cfa2
yendaravila <yendaravila@gmail.com>
2023-05-19 14:13:58 -0300

    filtros combinados

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const fechaBase = dataAmazing.fechaActual
 const eventos = dataAmazing.eventos
 const eventosPasados = []
 const eventosFuturos = []
-console.log(fechaBase)
-console.log(eventos)
+let checkedCheckboxes = []
+var arrayAFiltrar = []
+var arrayCheckbox = []
+var eventosFiltrados = []
+
 
 
 for (var i = 0; i < eventos.length; i++) {
@@ -32,16 +35,22 @@ function imprimir(id) {
 
         case "upcomingEvents":
             display(eventosFuturos)
+            arrayAFiltrar = eventosFuturos
+            eventsCategories(eventosFuturos)
             document.getElementById("TitulosPaginas").innerHTML = "UPCOMING EVENTS"
             break;
 
         case "pastEvents":
             display(eventosPasados)
+            arrayAFiltrar = eventosPasados
+            eventsCategories(eventosPasados)
             document.getElementById("TitulosPaginas").innerHTML = "PAST EVENTS"
             break;
 
         default:
             display(eventos)
+            arrayAFiltrar = eventos
+            eventsCategories(eventos)
             document.getElementById("TitulosPaginas").innerHTML = "HOME"
 
     }
@@ -62,6 +71,7 @@ function display(array) {
     `
     }
     document.getElementById("todosloseventos").innerHTML = html;
+    
 }
 
 imprimir("home");
@@ -81,23 +91,132 @@ switch (paginasDinamicas[1]) {
         break;
     default:
         imprimir("home")
-        document.getElementById("TitulosPaginas").innerHTML = "HOME"
 
+        document.getElementById("TitulosPaginas").innerHTML = "HOME"
+}
+
+var flechaDerecha = document.getElementById("derecho")
+
+var flechaIzquierda = document.getElementById("izquierdo")
+
+flechaDerecha.addEventListener("click", function (e) {
+    flechas()
+})
+flechaIzquierda.addEventListener("click", function (e) {
+    flechas()
+})
+
+function flechas() {
+    const paginasDinamicas = location.search.split("?page=");
+
+    if (paginasDinamicas[1] == "upcoming") {
+        flechaDerecha.href="./index.html?page=past"
+        flechaIzquierda.href="./index.html?page=home"
+    }
+    else if (paginasDinamicas[1] == "past") {
+        flechaDerecha.href="./paginas/Contact.html"
+        flechaIzquierda.href="./index.html?page=upcoming"
+    }
+    else{
+        flechaIzquierda.href="./paginas/stats.html"
+        flechaDerecha.href="./index.html?page=upcoming"
+    }
 }
 
 
-
-//Dinamismo del input
-
+// input
 
 var inputSearch = document.getElementById("inputSearch")
 
+inputSearch.addEventListener("keyup", function (evento) {
 
-inputSearch.addEventListener("keyup", function (evento) { capturaEvento(evento) })
+    var datoInput = evento.target.value
 
-function capturaEvento(evento) {
-    
-    
-   
+    var eventosFiltrados = arrayAFiltrar.filter(evento => evento.name.trim().toLowerCase().includes(datoInput.trim().toLowerCase()))
+
+    display(eventosFiltrados)
+    //filtrosCombinados() me da error
+
+})
+
+function eventsCategories(array) {
+
+    let categories = array.map(evento => evento.category)
+
+    let unica = new Set(categories)
+
+    let lastCategories = [...unica]
+
+    let categoriasEventos = ""
+    lastCategories.map(category =>
+        categoriasEventos +=
+        `
+        <label><input type="checkbox" value="${category}"> ${category}</label>
+        `
+    )
+    document.getElementById("checkBox").innerHTML = categoriasEventos
+
+    checkboxsListener()
+}
+
+function checkboxsListener() {
+
+    var checkboxs = document.querySelectorAll('input[type=checkbox')
+
+    for (i = 0; i < checkboxs.length; i++) {
+        checkboxs[i].addEventListener("change", function () {
+            checkedCheckboxes = []
+            for (i = 0; i < checkboxs.length; i++) {
+                if (checkboxs[i].checked) {
+                    checkedCheckboxes.push(checkboxs[i].value)
+                }
+            }
+            console.log(checkedCheckboxes)
+            console.log(arrayAFiltrar)
+            //filtrosCombinados()
+
+            var eventosPorCategorias = []
+            checkedCheckboxes.map(category => {
+                let prueba = arrayAFiltrar.filter(evento => evento.category === category)
+                eventosPorCategorias.push(...prueba)
+
+            })
+
+            display(eventosPorCategorias)
+
+
+        })
+
+    }
+}
+
+function filtrosCombinados() {
+
+    var eventosPorCategoria = [];
+
+    if (eventosFiltrados !== "" && arrayCheckbox.length > 0) {
+
+        arrayCheckbox.map(category => {
+            eventosPorCategoria.push(...arrayAFiltrar.filter(evento => evento.name.toLowerCase().includes(eventosFiltrados) && evento.category === category))
+        })
+
+    }
+    else if (eventosFiltrados !== "" && arrayCheckbox.length == 0) {
+        eventosPorCategoria = arrayAFiltrar.filter(evento => evento.name.toLowerCase().includes(eventosFiltrados))
+    }
+    else if (eventosFiltrados === "" && arrayCheckbox.length > 0) {
+
+        arrayCheckbox.map(category =>
+            eventosPorCategoria.push(...arrayAFiltrar.filter(evento => evento.category === category))
+        )
+    }
+
+    else {
+        eventosPorCategoria = arrayAFiltrar
+    }
+    eventosPorCategoria.length > 0 ?
+        display(eventosPorCategoria):
+
+        document.getElementById("todosloseventos").innerHTML = `<h1>NO SE ENCONTRARON EVENTOS PARA TU BUSQUEDA</h1>`
 
 }
